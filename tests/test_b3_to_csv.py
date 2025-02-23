5c98532167213a1b657e0c55bb5481964aebfef9
Silvas <ms.bruno1@gmail.com>
2025-02-23 09:21:06 -0300

    refactor: change resource directory name

--- tests/test_b3_to_csv.py
+++ tests/test_b3_to_csv.py
@@ -1,14 +1,19 @@
 import unittest
-import sys
-sys.path.append('../src')
-from b3_to_csv import B3CsvProcessor
+from src.b3_to_csv import B3CsvProcessor
+import os
+
+def get_resource(file):
+    return (os.path.join(os.path.dirname(__file__), 
+                   '',
+                   'resources', 
+                   'Teste_PY_Negociacao.xlsx'))
 
 class TestCalculadora(unittest.TestCase):
     def test_create_treated_dataframe_from_xlsx(self):
-        path_xlsx = './resource/Teste_PY_Negociacao.xlsx'
+        path_xlsx = get_resource('Teste_PY_Negociacao.xlsx')
         processor = B3CsvProcessor()
-        df = processor.create_treated_dataframe(path_xlsx)
-        print(df)
+        cei_data = processor.create_treated_dataframe(path_xlsx)
+        df = cei_data.df
         tickers = df['ticker'].unique()
         assert len(tickers) == 2 
         operations = df.groupby("ticker")['operacao'].count().to_dict()
